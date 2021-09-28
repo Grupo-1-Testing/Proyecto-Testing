49c6fcc5504bdf590b217ae971b0b97b745fe931
Antonia <absoto4@uc.cl>
2021-09-28 17:01:33 -0300

    Fixed probability of creating a mine
    Randomly selecting a sample of cells

--- lib/backend/board.rb
+++ lib/backend/board.rb
@@ -4,38 +4,33 @@ require_relative 'cell'
 
 # Represents the board of mineseaker
 class Board
-  attr_accessor :dimension, :number_mines, :cells, :counter_creation_mines
+  attr_accessor :dimension, :number_mines, :cells, :mine_cells
 
   def initialize(dimension, number_mines)
     @dimension = dimension
     @number_mines = number_mines
-    @counter_creation_mines = 0
+    @mine_cells = Array(0...@dimension * @dimension).sample(@number_mines).sort
     @cells = []
   end
 
   def create_board
-    (0...@dimension).each do |_i|
-      fila = []
-      (0...@dimension).each do |_j|
-        new_cell = create_cell
-        fila.append(new_cell)
+    (0...@dimension).each do |i|
+      row = []
+      (0...@dimension).each do |j|
+        cell_number = j + i * @dimension
+        new_cell = create_cell(cell_number)
+        row.append(new_cell)
       end
-      @cells.append(fila)
+      @cells.append(row)
     end
+  end
+
+  def create_cell(cell_number)
+    has_bomb = @mine_cells.include?(cell_number)
+    Cell.new(has_bomb, '', '')
   end
 
   def check_conditions; end
 
   def make_move; end
-
-  def create_cell
-    p = rand(1...100)
-    if p <= 50 && @counter_creation_mines < @number_mines
-      @counter_creation_mines += 1
-      cell = Cell.new(true, '', '')
-    else
-      cell = Cell.new(false, '', '')
-    end
-    cell
-  end
 end
